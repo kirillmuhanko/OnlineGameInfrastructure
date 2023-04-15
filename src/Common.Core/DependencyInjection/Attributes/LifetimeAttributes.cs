41970384c5e45ba7819893b778b1a71b67a8966d
kirillmuhanko <79082614+kirillmuhanko@users.noreply.github.com>
2023-04-15 15:58:20 +0300

    feat: dependency injection

---
+++ src/Common.Core/DependencyInjection/Attributes/LifetimeAttributes.cs
@@ -0,0 +1,18 @@
+﻿namespace Common.Core.DependencyInjection.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public abstract class LifetimeAttributeBase : Attribute
+{
+}
+
+public class TransientLifetimeAttribute : LifetimeAttributeBase
+{
+}
+
+public class ScopedLifetimeAttribute : LifetimeAttributeBase
+{
+}
+
+public class SingletonLifetimeAttribute : LifetimeAttributeBase
+{
+}
---
+++ src/Common.Core/DependencyInjection/Extensions/DependencyInjectionExtensions.cs
@@ -0,0 +1,48 @@
+﻿using System.Reflection;
+using Common.Core.DependencyInjection.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Core.DependencyInjection.Extensions;
+
+public static class DependencyInjectionExtensions
+{
+    private static readonly List<Type> AbstractionExclusionList = new()
+    {
+        typeof(IDisposable),
+        typeof(IAsyncDisposable)
+    };
+
+    public static void Scan(this IServiceCollection services, IEnumerable<Assembly> assemblies)
+    {
+        foreach (var assembly in assemblies)
+            services.AddServicesFromAssembly(assembly);
+    }
+
+    private static void AddServicesFromAssembly(this IServiceCollection services, Assembly assembly)
+    {
+        var implementations = assembly.GetTypes()
+            .Where(t => t.IsClass && !t.IsAbstract && t.GetCustomAttribute<LifetimeAttributeBase>() != null)
+            .ToList();
+
+        implementations.ForEach(i =>
+        {
+            var transient = i.GetCustomAttribute<TransientLifetimeAttribute>();
+            var scoped = i.GetCustomAttribute<ScopedLifetimeAttribute>();
+            var singleton = i.GetCustomAttribute<SingletonLifetimeAttribute>();
+
+            var abstractions = i.GetInterfaces()
+                .Where(a => !AbstractionExclusionList.Contains(a))
+                .ToList();
+
+            abstractions.ForEach(a =>
+            {
+                if (transient != null)
+                    services.AddTransient(a, i);
+                else if (scoped != null)
+                    services.AddScoped(a, i);
+                else if (singleton != null)
+                    services.AddSingleton(a, i);
+            });
+        });
+    }
+}
