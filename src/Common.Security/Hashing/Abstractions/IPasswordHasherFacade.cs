d9bb9c5e947682a8e73e7fa3c245f71d6fc524bd
kirillmuhanko <79082614+kirillmuhanko@users.noreply.github.com>
2023-04-15 18:36:15 +0300

    feat: password hasher

---
+++ src/Common.Security/Hashing/Abstractions/IPasswordHasherFacade.cs
@@ -0,0 +1,8 @@
+﻿namespace Common.Security.Hashing.Abstractions;
+
+public interface IPasswordHasherFacade
+{
+    string HashPassword(string password);
+
+    bool VerifyHashedPassword(string hashedPassword, string enteredPassword);
+}
---
+++ src/Common.Security/Hashing/Implementations/PasswordHasherFacade.cs
@@ -0,0 +1,28 @@
+﻿using Common.Core.DependencyInjection.Attributes;
+using Common.Security.Hashing.Abstractions;
+using Microsoft.AspNetCore.Identity;
+
+namespace Common.Security.Hashing.Implementations;
+
+[SingletonLifetime]
+public class PasswordHasherFacade : IPasswordHasherFacade
+{
+    private readonly PasswordHasher<string> _passwordHasher;
+
+    public PasswordHasherFacade()
+    {
+        _passwordHasher = new PasswordHasher<string>();
+    }
+
+    public string HashPassword(string password)
+    {
+        var hashedPassword = _passwordHasher.HashPassword(null!, password);
+        return hashedPassword;
+    }
+
+    public bool VerifyHashedPassword(string hashedPassword, string enteredPassword)
+    {
+        var result = _passwordHasher.VerifyHashedPassword(null!, hashedPassword, enteredPassword);
+        return result == PasswordVerificationResult.Success;
+    }
+}
