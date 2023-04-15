369ebde4863b601982f6fd569730176822b4446c
kirillmuhanko <79082614+kirillmuhanko@users.noreply.github.com>
2023-04-15 19:32:22 +0300

    feat: integration test context

---
+++ src/Common.Core/DependencyInjection/Implementations/IntegrationTestContext.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using Common.Core.DependencyInjection.Extensions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Core.DependencyInjection.Implementations;
+
+public class IntegrationTestContext : IDisposable
+{
+    private readonly IServiceProvider _serviceProvider;
+    private readonly IServiceScope _serviceScope;
+
+    public IntegrationTestContext(IEnumerable<Assembly> assemblies)
+    {
+        var builder = WebApplication.CreateBuilder(Array.Empty<string>());
+        builder.Services.Scan(assemblies);
+        var app = builder.Build();
+        _serviceScope = app.Services.CreateScope();
+        _serviceProvider = _serviceScope.ServiceProvider;
+    }
+
+    public TService ResolveService<TService>() where TService : class
+    {
+        var service = _serviceProvider.GetRequiredService<TService>();
+        return service;
+    }
+
+    public void Dispose()
+    {
+        _serviceScope.Dispose();
+    }
+}
